4889b9571e1ce6a424e4d9bfc25e776c87e6cc6c
Felipe Finhane de Paula <felipefinhane@gmail.com>
2017-12-04 10:55:22 -0200

    Teste com integracao da base de dados do mongoDB Heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var app = require('./config/express')();
-require('./config/database.js')('mongodb://localhost/contatooh');
+// require('./config/database.js')('mongodb://localhost/contatooh');
+require('./config/database.js')('mongodb://contatooh:123contatooh@ds129796.mlab.com:29796/heroku_hwxb31nl');
+
 
 http.createServer(app)
     .listen(app.get('port'), function() {
