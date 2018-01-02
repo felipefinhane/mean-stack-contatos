1be6c5f89ae16e66254b6e0d67e9b37253f87798
Felipe Finhane de Paula <felipefinhane@gmail.com>
2018-01-02 14:28:47 -0200

    Add helmet

--- config/express.js
+++ config/express.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
+var helmet = require('helmet');
 
 module.exports = function() {
     var app = express();
@@ -27,6 +28,12 @@ module.exports = function() {
     ));
     app.use(passport.initialize());
     app.use(passport.session());
+    //HELMET
+    app.use(helmet.xframe());
+    app.use(helmet.xssFilter());
+    app.use(helmet.nosniff());
+    //app.use(helmet.hidePoweredBy({setTo : 'PHP 3.8.1'}));
+    app.disable('x-powered-by');
     //AUTOLOAD models;controllers;routes - Ordem importa
     load('models', {cwd: 'app'})
         .then('controllers')
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var http = require('http');
 var app = require('./config/express.js')();
 require('./config/passport.js')();
-// require('./config/database.js')('mongodb://localhost/contatooh');
+require('./config/database.js')('mongodb://172.17.0.2/contatooh');
 //MongoDB Heroku
-require('./config/database.js')('mongodb://contatooh:123contatooh@ds129796.mlab.com:29796/heroku_hwxb31nl');
+//require('./config/database.js')('mongodb://contatooh:123contatooh@ds129796.mlab.com:29796/heroku_hwxb31nl');
 http.createServer(app)
     .listen(app.get('port'), function() {
         console.log("Express Server listening on port: " + app.get('port'));
